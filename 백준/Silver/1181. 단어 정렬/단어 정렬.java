d61e181fbd3adb8e691b3aa13711cbe204c91c48
SEONGCHAN CHOI <158448446+Seongchan-CH0I@users.noreply.github.com>
2025-08-01 10:40:55 +0900

    [Silver V] Title: 단어 정렬, Time: 376 ms, Memory: 23432 KB -BaekjoonHub

---
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+
+        HashSet<String> set = new HashSet<>();
+        for (int i = 0; i < N; i++) {
+            String s = br.readLine();
+            set.add(s);
+        }
+
+        ArrayList<String> list = new ArrayList<>(set);
+        list.sort((s1,s2) -> {
+            if (s1.length() != s2.length()) {
+                return s1.length() - s2.length();
+            }
+            return s1.compareTo(s2);
+        });
+
+        for (int i = 0; i < list.size(); i++) {
+            bw.write(list.get(i) + "\n");
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
